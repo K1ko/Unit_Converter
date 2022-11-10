793492ea7016af7d05a3abe3b05c820fe1a30ddf
Kristián Červenka <kristian.cervenka@gmail.com>
2022-11-10 20:26:41 +0100

    Added switch case

--- main.c
+++ main.c
@@ -10,6 +10,14 @@
  #         Slovak Technical University         #
 //##############################################
  */
+void currency()
+{}
+void length()
+{}
+void weight()
+{}
+void temperature()
+{}
 int main() {
     printf("Hello, World!\n");
     int a=1;
@@ -17,5 +25,24 @@ int main() {
     int sum = a+b;
     printf("Sum is %d", sum);
     //toto je test
+    char vetva;
+    do {
+        scanf("%c", &vetva); //načítanie vetvy
+        switch (vetva) {
+            case 'v': //postupné volanie funkcii
+                currency();
+                break;
+            case 'o':
+                length();
+                break;
+            case 'h':
+                weight();
+                break;
+            case 't':
+                temperature();
+                break;
+
+        }
+    } while (vetva != 'k');
     return 0;
 }//1
